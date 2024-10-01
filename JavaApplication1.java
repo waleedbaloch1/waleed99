2425965c146a7ab468a1cf6dd7d63d61847104b6
waleedbaloch1 <796628@pdsb.net>
2024-10-01 10:50:34 -0400

    Update JavaApplication1.java

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -27,39 +27,39 @@ public class JavaApplication2 {
         Scanner sc = new Scanner (System.in); // putting 
         System.out.println("width is "); // input for width
         width = sc.nextDouble(); // scans for width
-        System.out.println(" length is  "); // input for length
+        System.out.printf(" length is  "); // input for length
         length = sc.nextDouble(); // scans for length
-        System.out.println(length*width);
-        System.out.println("that is the area"); // stating taht is the area
+        System.out.printf(length*width);
+        System.out.printf("that is the area"); // stating that is the area
         double radius;
         double diameter;
-        System.out.println("radius is: ");
-         radius = sc.nextDouble();
-         diameter = (radius*radius);
-         System.out.println(Math.PI*diameter);
-         System.out.println("that is the area of a circle");
+        System.out.println("radius is: "); // asks the user to input the radius
+         radius = sc.nextDouble(); // defines the radius
+         diameter = (radius*radius); // the radius
+         System.out.printf(Math.PI*diameter); // prints out the area
+         System.out.printf("that is the area of a circle"); // prints out the circle
          double radius2;
          double height;
-          System.out.printf("radius is: ");
+          System.out.printf("radius is: "); // 
           radius2 = sc.nextDouble();
-          System.out.println("height is:");
+          System.out.printf("height is:");
           height = sc.nextDouble();
           double diameter2 = (radius*radius);
-          System.out.println("the Volume is ");
-          System.out.println(Math.PI*diameter2*height);
-          System.out.println("the surface area is");
+          System.out.printf("the Volume is ");
+          System.out.printf(Math.PI*diameter2*height);
+          System.out.printf("the surface area is");
           double Bye = (2*Math.PI*radius2*height);
           double Hello = (2*Math.PI*radius2*diameter2);
-          System.out.print(Bye+Hello);
+          System.out.printf(Bye+Hello);
           double g = 9.8;
           double L;
-            System.out.println("meters are: ");
+            System.out.printf("meters are: ");
             L = sc.nextDouble();
            double No = (g*L);
            double square = (Math.sqrt(No));
            Double Ta = (Math.PI*2);
-           System.out.println("Time for one pendulum swing");
-           System.out.println(square*Ta);
+           System.out.printf("Time for one pendulum swing");
+           System.out.printf(square*Ta);
           
           
           
