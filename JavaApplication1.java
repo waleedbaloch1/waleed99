66ae1bb440f6627088b47a4264c552688605e027
waleedbaloch1 <796628@pdsb.net>
2024-10-01 18:07:33 -0400

    Update JavaApplication1.java

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -40,26 +40,26 @@ public class JavaApplication2 {
          System.out.println("that is the area of a circle"); // prints out the circle
          double radius2;
          double height;
-          System.out.println("radius for cylinder: "); //  
-         radius2 = sc.nextDouble();
-          System.out.println("height of cylinder is: ");
-          height = sc.nextDouble();
-          double diameter2 = (radius*radius);
-          System.out.println("the Volume is ");
-          System.out.println(Math.PI*diameter2*height);
-          System.out.println("the surface area is");
-          double Bye = (2*Math.PI*radius2*height);
-          double Hello = (2*Math.PI*radius2*diameter2);
-          System.out.println(Bye+Hello);
-          double g = 9.8;
-          double L;
-            System.out.println("how long is your pendulum in meters: ");
-            L = sc.nextDouble();
-           double No = (g*L);
-           double square = (Math.sqrt(No));
-           Double Ta = (Math.PI*2);
-           System.out.println("Time for one pendulum swing");
-           System.out.println(square*Ta);
+          System.out.println("radius for cylinder: "); // defines the radius for cylinder
+         radius2 = sc.nextDouble(); // adds decimals
+          System.out.println("height of cylinder is: "); // defines the height of cylinder
+          height = sc.nextDouble(); // adds decimals of height
+          double diameter2 = (radius*radius); // radius times radius
+          System.out.println("the Volume is "); // defines the volume
+          System.out.println(Math.PI*diameter2*height); // PI times diameter times height
+          System.out.println("the surface area is"); // defines the surface area
+          double Bye = (2*Math.PI*radius2*height); // adds decimals and PI times radius times height
+          double Hello = (2*Math.PI*radius2*diameter2); // PI times radius times diameter
+          System.out.println(Bye+Hello); // these are my variables
+          double g = 9.8; // decimals
+          double L; // adds decimals
+            System.out.println("how long is your pendulum in meters: "); // defines how is the pendulum in meters
+            L = sc.nextDouble(); // adds decimals
+           double No = (g*L); 
+           double square = (Math.sqrt(No)); // square root times my variable
+           Double Ta = (Math.PI*2); 
+           System.out.println("Time for one pendulum swing"); // defines pendulum swing
+           System.out.println(square*Ta); // Square times Ta
           
           
           
